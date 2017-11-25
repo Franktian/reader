657c73da7d8bf9812930a411cb597dde3c300267
Frank <tianyawen201209@hotmail.com>
2017-11-25 16:22:47 -0500

    Create user table

--- src/server/user/models.py
+++ src/server/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """
+    User table
+    """
+    user_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
