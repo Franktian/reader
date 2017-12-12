2fcb08ad5a9f4309664119477e9f3ac3810d2c89
Frank <tianyawen201209@hotmail.com>
2017-12-11 20:26:36 -0500

    Added db url as environment

--- src/server/config/settings.py
+++ src/server/config/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import environ
 
+env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -96,14 +98,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'legacy',
-        'USER': 'franktian',
-        'PASSWORD': '',
-        'HOST': '192.168.10.236',
-        'PORT': '5432',
-    }
+    'default': env.db("DATABASE_URL")
 }
 
 
