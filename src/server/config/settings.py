39865782cdc9cfdc4ff4ac9864a5eb3378b0718c
Frank <tianyawen201209@hotmail.com>
2017-12-20 22:09:29 -0500

    Added environment reading

--- src/server/config/settings.py
+++ src/server/config/settings.py
@@ -16,7 +16,7 @@ import environ
 env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+environ.Env.read_env('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
